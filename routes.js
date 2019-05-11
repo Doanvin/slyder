76871d778a98e6ea0e02a4094fcb5220d65d3295
Doanvin <donavinhannon@gmail.com>
2019-05-11 14:37:29 -0700

    Updated routes to work with imported html

--- routes.js
+++ routes.js
@@ -14,13 +14,22 @@ page({
 
 function replaceContent(id) {
     if ('content' in document.createElement('template')){
+
+        // $content is the element who's content you would like to replace
         let $content = document.getElementById('main-content');
-        const template = document.getElementById(id);
+
+        // access the link import
+        const link = document.querySelector('link[rel=import]').import;
+
+        // select the template from inside the imported html
+        const template = link.getElementById(id);
+
+        // clone it, clear the $content, insert your cloned template to $content
         const clone = document.importNode(template.content, true);
         $content.innerHTML = '';
         $content.appendChild(clone);
     } else {
-        alert('HTML template tags are not supported by your browser. Please upgrade to Firefox or Chrome.')
+        alert('HTML template tags are not supported by your browser. Please upgrade to the latest version of Firefox or Chrome.')
     }
 }
 
---
+++ views/views.js
@@ -0,0 +1,4 @@
+const docs = {
+    title: 'Documentation for Slyder',
+    description: 'This is a description of the documentation page for Slyder. I would like this section to be about 60 chars.'
+}
