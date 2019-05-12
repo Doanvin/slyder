e3c28e44c3e3decb6403464d7a6045d7aabc9e8d
Doanvin <donavinhannon@gmail.com>
2019-05-11 19:36:38 -0700

    moved sctipts from templates into routes middleware

--- routes.js
+++ routes.js
@@ -10,7 +10,17 @@ page({
     hashbang:true
 });
 
+// Set the metadata for the new page
+// meta is an object with title and description keys
+function setMetadata(meta) {
+    let title = document.getElementsByTagName('title')[0],
+        description = document.querySelector('meta[description]');
 
+    title.setAttribute('title', meta.title);
+    description.setAttribute('description', meta.description);
+}
+
+// replace the content of the of the element with id arg
 function replaceContent(id) {
     if ('content' in document.createElement('template')){
 
@@ -30,22 +40,47 @@ function replaceContent(id) {
 }
 
 function index() {
+    const metadata = {
+        title: 'Slyder | Modern Page Transitions',
+        description: 'A simple page transition component for single page applications.'
+    }
+    setMetadata(metadata);
     replaceContent('index');
 }
 
 function usage() {
+    const metadata = {
+        title: 'Slyder | Usage',
+        description: 'How to use Slyder page transitions for single page applications. When to use Slyder.'
+    }
+    setMetadata(metadata);
     replaceContent('usage');
 }
 
 function docs() {
+    const metadata = {
+        title: 'Slyder | Documentation',
+        description: 'Learn about Slyder page transitions for single page applicaitons. Use Slyder for a superior UX.'
+    }
+    setMetadata(metadata);
     replaceContent('docs');
 }
 
 function example() {
+    const metadata = {
+        title: 'Slyder | Examples',
+        description: 'A walk through of Slyder page transitions being used. Look at the code.'
+    }
+    setMetadata(metadata);
     replaceContent('example');
 }
 
 function notfound() {
+    const metadata = {
+        title: 'Slyder | 404 Page Not Found',
+        description: 'Slyder page transitions for single page applications 404 page.'
+    }
+    setMetadata(metadata);
     replaceContent('notfound');
 }
 
